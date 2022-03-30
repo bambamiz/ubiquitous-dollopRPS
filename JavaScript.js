8dc04ca39138edfa5cfa3dd294ee5f470a87255d
bambamiz <bambam-24@live.com>
2022-03-30 19:43:24 -0400

    added a funtion computerPlay to generate a random number between 0 and 2

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const max = 3; //max should always be three, this is what the random return value is based on.
+function computerPlay (a)
+{
+    
+    let x = Math.floor(Math.random() * max);
+
+    return x;
+}
+
+console.log(computerPlay(max));
